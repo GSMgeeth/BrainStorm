87e01161a1344485ca52c9263946909e3624ccd5
Geeth Sandaru Madhushan <geeth.gsm@gmail.com>
2018-03-23 21:11:18 +0530

    making of the clock interface

---
+++ Clock.cpp
@@ -0,0 +1,37 @@
+// BrainStorm.cpp : Defines the entry point for the console application.
+// project for keeping the brain active
+
+#include "stdafx.h"
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+
+int main(void)
+{
+	printf("        12      ");
+	
+	printf("\n");
+	printf("    11      01    ");
+	
+	printf("\n\n");
+	printf(" 10           02 ");
+	
+	printf("\n\n");
+	printf("09      .      03");
+	
+	printf("\n\n");
+	printf(" 08           04 ");
+	
+	printf("\n\n");
+	printf("    07      05    ");
+	
+	printf("\n");
+	printf("        06      ");
+
+	char c;
+	scanf_s("%c", &c);
+
+}
+
